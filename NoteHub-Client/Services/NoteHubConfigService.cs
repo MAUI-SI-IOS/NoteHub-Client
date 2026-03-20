b309fe38bbe2cfbc347c4d681353e3a7d53f0333
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-20 14:29:06 -0400

    Create service for managing preferences

---
+++ NoteHub-Client/Services/NoteHubConfigService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteHub_Client.Services
+{
+    public class NoteHubConfigService : INoteHubConfigService
+    {
+        const string SERVER_URL_PREFERENCE_KEY = "nothub_servel_url";
+        public string? ServerConnectionUrl
+        {
+            get => Preferences.Get(SERVER_URL_PREFERENCE_KEY, null);
+
+            set
+            {
+                if (value != null)
+                    Preferences.Set(SERVER_URL_PREFERENCE_KEY, value);
+                else
+                    Preferences.Remove(SERVER_URL_PREFERENCE_KEY);
+            }
+        }
+    }
+}
