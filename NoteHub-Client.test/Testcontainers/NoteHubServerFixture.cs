51004c76ecbcc77a1f4c8b82ee1dfc9e21d380db
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-03-25 01:50:41 -0400

    add project for testing and create a fixture to setup an NoteHubServer for tests

---
+++ NoteHub-Client.test/Testcontainers/NoteHubServerFixture.cs
@@ -0,0 +1,60 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
+using Testcontainers.PostgreSql;
+
+namespace NoteHub_Client.test.Testcontainers
+{
+    public class NoteHubServerFixture : IAsyncLifetime
+    {
+        const string DATABASE_NAME = "notehubDB";
+        const string DATABASE_USER = "notehub";
+        const string DATABASE_PASSWORD = "password";
+
+        private readonly INetwork network;
+        private readonly PostgreSqlContainer db;
+        private readonly IContainer api;
+
+        public NoteHubServerFixture()
+        {
+            network = new NetworkBuilder()
+                .WithName("notehub_testing")
+                .WithCleanUp(true)
+                .Build();
+
+            db = new PostgreSqlBuilder("postgres:latest")
+                .WithAutoRemove(true)
+                .WithNetwork(network)
+                .WithDatabase(DATABASE_NAME)
+                .WithUsername(DATABASE_USER)
+                .WithPassword(DATABASE_PASSWORD)
+                .Build();
+
+            api = new ContainerBuilder("ghcr.io/maui-si-ios/notehub-server:latest")
+                .WithImagePullPolicy(_ => true)
+                .WithAutoRemove(true)
+                .WithNetwork(network)
+                .WithEnvironment("POSTGRES_HOST", db.Hostname)
+                .WithEnvironment("POSTGRES_DB", DATABASE_NAME)
+                .WithEnvironment("POSTGRES_USER", DATABASE_USER)
+                .WithEnvironment("POSTGRES_PASSWORD", DATABASE_PASSWORD)
+                .WithPortBinding(8080, true)
+                .DependsOn(db)
+                .Build();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await api.DisposeAsync();
+            await db.DisposeAsync();
+            await network.DisposeAsync();
+        }
+
+        public async ValueTask InitializeAsync()
+        {
+            await network.CreateAsync();
+            await db.StartAsync();
+            await api.StartAsync();
+        }
+    }
+}
---
+++ NoteHub-Client.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NoteHub_Client.test.Testcontainers;
+
+namespace NoteHub_Client.test
+{
+    public class UnitTest1 : IClassFixture<NoteHubServerFixture>
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
