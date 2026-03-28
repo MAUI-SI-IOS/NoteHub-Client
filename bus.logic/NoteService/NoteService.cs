80d3821eedab8984edfadd1b8b39ad3184418194
william <descoteaux.william@gmail.com>
2026-03-28 19:53:18 -0400

    logic des Get Methods rajouter

---
+++ bus.logic/NoteService/NoteService.cs
@@ -0,0 +1,50 @@
+﻿
+
+using bus.logic.ApiService;
+using bus.logic.ApiService.Directors;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace bus.logic.service
+{
+    public class NoteService
+    {
+        Api api;
+        public NoteService(HttpClient client)
+        {
+            api = new Api(client);
+        }
+
+        public Task<List<Note>> GetNotes()
+        {
+            return api.Send(new GetDirector<List<Note>>("/note/"));
+        }
+
+        public Task<Note> GetNoteByTitle(string title)
+        {
+            return api.Send(new GetDirector<Note>("note/title/" + title));
+        }
+
+
+        public void AddNote(Note note)
+        {
+            api.Send(new PostNoteDirector<Note>(note));
+        }
+
+        //public void UpdateNote()
+        //{
+        //return api.Send<Note>(new PatchNoteDirector());
+        //}
+    }
+
+
+    public class Note
+    {
+        [JsonPropertyName("id")]
+        string? id;
+        [JsonPropertyName("title")]
+        string title;
+        [JsonPropertyName("raw_content")]
+        string RawContent;
+    }
+}
