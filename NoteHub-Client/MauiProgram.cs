7559632fc1fbb1b702b2a65f374865a69c42d256
william <descoteaux.william@gmail.com>
2026-04-04 15:35:12 -0400

    added DI

--- NoteHub-Client/MauiProgram.cs
+++ NoteHub-Client/MauiProgram.cs
@@ -1,5 +1,11 @@
-﻿using CommunityToolkit.Maui;
+﻿using bus.logic.NoteService;
+using bus.logic.Result;
+using bus.logic.service;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Alerts;
 using Microsoft.Extensions.Logging;
+using NoteHub_Client.Services.Config;
+using NoteHub_Client.ViewModels;
 
 
 namespace NoteHub_Client
@@ -18,8 +24,29 @@ namespace NoteHub_Client
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton<INoteHubConfigService, NoteHubConfigService>();
+            //check if config changed and use the appropriate service
+            builder.Services.AddTransient<INoteService>(container =>
+            {
+                var config = container.GetRequiredService<INoteHubConfigService>();
+
+                return config.Client.Match<HttpClient, string, INoteService>(
+                    ok: (client) => new ServerNoteService(client),
+                    err:(dbPath) => new LocalNoteService(dbPath)
+                );
+            });
+
+            
+            builder.Services.AddTransient<WriteNotePage>();
+            builder.Services.AddTransient<WriteNoteViewModel>();
+
+            builder.Services.AddTransient<SearchPage>();
+            builder.Services.AddTransient<SearchNoteViewModel>();
+
+            builder.Services.AddTransient<ServerSelectionPage>();
+            builder.Services.AddTransient<ServerSelectionViewModel>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
