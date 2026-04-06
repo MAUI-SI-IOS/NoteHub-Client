0bf8f02217b70a0f19890f442609f1dc183063bc
william <descoteaux.william@gmail.com>
2026-04-06 18:34:09 -0400

    seperate Note model from ServerService into its own file and folder for clarity

---
+++ bus.logic/models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace bus.logic.models
+{
+    public class Note
+    {
+        [JsonPropertyName("id")]
+        public long? Id { get; set; } // Matches Long? = null
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } // Matches val title: String
+
+        [JsonPropertyName("rawContent")] // WAS raw_content (Fixed to camelCase)
+        public string RawContent { get; set; }
+
+        [JsonPropertyName("formattedContent")] // WAS formated_content (Fixed spelling + case)
+        public string FormattedContent { get; set; }
+        public Note() { }
+        public Note(string title, string note, long? id = null)
+        {
+            Title = title;
+            RawContent = note;
+            FormattedContent = note;
+            Id = id;
+        }
+    }
+}
