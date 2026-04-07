0a68b0f059ad3abc717318e08f9abd9375ef3d55
william <descoteaux.william@gmail.com>
2026-04-06 21:29:57 -0400

    simplified proxy

--- bus.logic/NoteService/ProxyNoteService.cs
+++ bus.logic/NoteService/ProxyNoteService.cs
@@ -2,93 +2,42 @@
 using bus.logic.models;
 using bus.logic.Result;
 using bus.logic.service;
+using System.Data;
 using System.Diagnostics;
 
 
 namespace bus.logic.NoteService
 {
 
-    public class ProxyNoteService : INoteService, IDisposable 
+    public class ProxyNoteService : INoteService
     {
         private readonly SoftZip _zip;
-        
-        private bool IsUpgraded;
+        private bool _isOnline = true;
         public event Action<bool> OnStatusChanged;
-
-        private CancellationTokenSource _cts;
-
 
         public ProxyNoteService(INoteService fallback, INoteService upgraded)
         {
             _zip = new SoftZip(fallback, upgraded);
-            Monitor();
+            _zip.OnStatusChanged += UpdateStatus;
         }
-
+        private void UpdateStatus(bool status)
+        {
+            if (_isOnline != status)
+            {
+                _isOnline = status;
+                OnStatusChanged?.Invoke(_isOnline);
+            }
+        }
 
         //--------------------//
         //    INoteService    //
         //--------------------//
         public async Task<Result<List<Note>, HttpException>> SearchNote(string token)
-        {
-            return await _zip.SearchNote(token);        
-        }
-
+        => await _zip.SearchNote(token);
         public async Task<Result<Note, HttpException>> GetNoteByTitle(string title)
-        {
-            return await _zip.GetNoteByTitle(title);
-        }
-
+        => await _zip.GetNoteByTitle(title);
         public async Task<Result<Note, HttpException>> CreateUpdateNote(long? id, string title, string note)
-        {
-            //try to write localy if it works try to create if it works try to write
-            return await _zip.CreateUpdateNote(id, title, note);
-         }
-
-        public Task<bool> Ping()
-        {
-            return _zip.Ping();
-        }
-        
-
-        private void Monitor()
-        {
-            UpdateStatus(false);
-            _cts?.Cancel();
-            _cts = new CancellationTokenSource();
-            var token = _cts.Token;
-            Task.Run(async () =>
-            {
-                try
-                {
-                    while (!token.IsCancellationRequested)
-                    {
-                        Debug.WriteLine("[WAITING] for response");
-                        var result = await _zip.Ping();
-                        if (result)
-                        {
-                            UpdateStatus(true);
-                            break;
-                        }
-                        await Task.Delay(2000, token);
-                    }
-                }
-                catch (OperationCanceledException) { }
-            }, token);
-        }
-        private void UpdateStatus(bool status)
-        {
-            IsUpgraded = status;
-            OnStatusChanged?.Invoke(IsUpgraded);
-        }
-
-        public void Dispose()
-        {
-            _cts?.Cancel();
-            _cts?.Dispose();
-        }
-        ~ProxyNoteService()
-        {
-            Dispose();
-        }
+        => await _zip.CreateUpdateNote(id, title, note);
+        public Task<bool> Ping() => _zip.Ping();
     }
 }
--- bus.logic/NoteService/SoftZip.cs
+++ bus.logic/NoteService/SoftZip.cs
@@ -13,7 +13,8 @@ namespace bus.logic.NoteService
     internal class SoftZip: INoteService
     {
         INoteService A; 
-        INoteService B; 
+        INoteService B;
+        public event Action<bool> OnStatusChanged;
         public SoftZip(INoteService a, INoteService b)
         {
             A = a;
@@ -25,31 +26,19 @@ namespace bus.logic.NoteService
             if (!localResult.IsSuccess) return localResult;
 
             return await B.CreateUpdateNote(id, title, note)
-                .BindErrAsync(async (ex) =>
-                {
-                    if (ex.code < 10)
-                    {
-                        return localResult;
-                    }
-                    return Result<Note, HttpException>.Failure(ex);
-                });
+                .TriggerErrAsync(async (ex) => { if (ex.code < 10) OnStatusChanged.Invoke(true); });
         }
         public async Task<Result<List<Note>, HttpException>> SearchNote(string token)
         {
             return await B.SearchNote(token)
-            .BindErrAsync(async (ex) => 
-            ex.code < 10 
-                ? await A.SearchNote(token) 
-                : Result<List<Note>, HttpException>.Failure(ex));
-
+                .TriggerErrAsync(async(ex)=> { if (ex.code < 10) OnStatusChanged.Invoke(true); })
+                .MapErrAsync(async (ex) => await A.SearchNote(token));
         }
         public async Task<Result<Note, HttpException>> GetNoteByTitle(string title)
         {
             return await B.GetNoteByTitle(title)
-            .BindErrAsync(async (ex) => 
-            ex.code < 10 
-                ? await A.GetNoteByTitle(title) 
-                : Result<Note, HttpException>.Failure(ex));
+                .TriggerErrAsync(async (ex) => { if (ex.code < 10) OnStatusChanged.Invoke(true); })
+                .MapErrAsync(async (ex) => await A.GetNoteByTitle(title));
         }
 
         public async Task<bool> Ping() => await B.Ping();
